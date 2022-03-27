fa7d2fe1d9626bf2a6d641da034a4e5c13558eb6
Guanyu Zhu <47459397+ChamPro@users.noreply.github.com>
2022-03-27 00:08:51 -0400

    Update difficulty.h

--- difficulty.h
+++ difficulty.h
@@ -4,12 +4,84 @@
 #include <iostream>
 #include <string>
 
-class difficulty {
+class Difficulty {
   public:
     virtual std::string getblock() = 0;
-    virtual ~difficulty();
+    virtual ~Difficulty();
 };
 
 
+class Level: public Difficulty {
+    protected:
+    Difficulty* diff;
+    public:
+    Level(Difficulty* diff);
+    virtual ~Level();
+    std::string getblock() override;
+};
+
+
+class Level: public Difficulty {
+    protected:
+    Difficulty* diff;
+    public:
+    Level(Difficulty* diff);
+    virtual ~Level();
+    std::string getblock() override;
+};
+
+class Level0: public Level {
+    protected:
+    Difficulty* diff;
+    public:
+    Level0(Difficulty* diff);
+    virtual ~Level0();
+    std::string getblock() override;
+};
+
+class Level1: public Level {
+    protected:
+    Difficulty* core;
+    public:
+    Level1(Difficulty* diff);
+    virtual ~Level1();
+    std::string getblock() override;
+};
+
+class Level2: public Level {
+    protected:
+    Difficulty* diff;
+    public:
+    Level2(Difficulty* diff);
+    virtual ~Level2();
+    std::string getblock() override;
+};
+
+class Level3: public Level {
+    protected:
+    Difficulty* diff;
+    public:
+    Level3(Difficulty* diff);
+    virtual ~Level3();
+    std::string getblock() override;
+};
+
+class Level4: public Level {
+    protected:
+    Difficulty* diff;
+    public:
+    Level4(Difficulty* diff);
+    virtual ~Level4();
+    std::string getblock() override;
+};
+
+class ActionHeavy: public Level {
+    protected:
+    Difficulty* diff;
+    public:
+    ActionHeavy(Difficulty* diff);
+    virtual ~ActionHeavy();
+    std::string getblock() override;
+};
 
 #endif

f96db890d106144a60b6f0b85795399befb0be15
Guanyu Zhu <47459397+ChamPro@users.noreply.github.com>
2022-03-27 10:52:07 -0400

    Update difficuly.cc

--- difficuly.cc
+++ difficuly.cc
@@ -4,12 +4,46 @@
 
 Difficulty::~Difficulty() { }
 
-Level::Level(Difficulty* diff): diff{diff} {
-    
-}
+Level::Level(Difficulty* diff): diff{diff} { }
 
 Level::~Level() { 
     delete this->diff;
 }
 
 std::string Level::getblock() { }
+
+Level0::Level0(Difficulty* diff): Level{diff} { }
+
+Level0::~Level0() { }
+
+std::string Level0::getblock() { }
+
+Level1::Level1(Difficulty* diff): Level{diff} { }
+
+Level1::~Level1() { }
+
+std::string Level1::getblock() { }
+
+Level2::Level2(Difficulty* diff): Level{diff} { }
+
+Level2::~Level2() { }
+
+std::string Level2::getblock() { }
+
+Level3::Level3(Difficulty* diff): Level{diff} { }
+
+Level3::~Level3() { }
+
+std::string Level3::getblock() { }
+
+Level4::Level4(Difficulty* diff): Level{diff} { }
+
+Level4::~Level4() { }
+
+std::string Level4::getblock() { }
+
+ActionHeavy::ActionHeavy(Difficulty* diff): Level{diff} {}
+
+ActionHeavy::~ActionHeavy() {}
+
+std::string ActionHeavy::getblock() { }

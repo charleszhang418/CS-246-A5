0c9b915f10bca382549656c24432390540a9fba6
Charles Zhang <charleszhang418@hotmail.com>
2022-03-28 21:09:36 -0400

    Temp main

---
+++ main.cc
@@ -0,0 +1,17 @@
+#include "command.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+int main () {
+
+    Command temp_com;
+    int width = 11, height = 18;
+    int cur_level = 0;
+    int hi_score = 0;
+
+}

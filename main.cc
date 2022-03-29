9548e2f505360dbb42790065102414e351b1e841
Charles Zhang <charleszhang418@hotmail.com>
2022-03-28 21:59:03 -0400

    Update
    3.28 22:00

--- main.cc
+++ main.cc
@@ -7,11 +7,59 @@
 
 using namespace std;
 
-int main () {
-
+int main (int argc, char *argv[]) {
+    // Initialization of variables
     Command temp_com;
     int width = 11, height = 18;
     int cur_level = 0;
     int hi_score = 0;
+
+    bool graphicsMode = true; // Default: with both graphic output and text output
+
+
+    // Command-line interface if exists
+    for (int i = 0; i < argc; ++i) {
+        string cmd_arg = argv[i];
+        if (cmd_arg == "-text") {
+            graphicsMode = false;
+        } 
+        if (cmd_arg == "-seed") {
+            // Left to add
+        }
+        if (cmd_arg == "scriptfile1") {
+            // set player1 to file
+        } 
+        if (cmd_arg == "scriptfile2") {
+            // set player2 to file;
+        } 
+        if (cmd_arg == "-startlevel") {
+            string xxx = argv[i + 1];
+            cur_level = stoi(xxx);
+            i++;
+        }
+
+    }
+
+    // Initialize boards with other settings
+    // NEED BOARD FULL header file
+
+
+    
+
+    // Game
+    while (true) {
+
+        bool restart = false;
+        // Two players turn
+        // With input
+
+        // Special Action
+
+        
+
+
+    }
+
+
 
 }
--- output.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OUTPUT_H_
-#define _OUTPUT_H_
-
-#include "socre.h"
-#include "command.h"
-
-
-#endif
-

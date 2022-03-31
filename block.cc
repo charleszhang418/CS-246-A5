d75dfb6d850986d0a59af027c66b97f48c523cf9
ChamPro <marooonfi@gmail.com>
2022-03-30 20:51:38 -0400

    rotate

--- block.cc
+++ block.cc
@@ -255,6 +255,143 @@ char LBlock::getChar() {
 
 std::vector<std::vector<int>> LBlock::rotate(bool clockwise) {
     std::vector<std::vector<int>> result;
+    if ((this->type == 0) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() - 1);
+        v1.emplace_back(this->cell1->getrow() + 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol());
+        v2.emplace_back(this->cell2->getrow() - 2);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() - 1);
+        v3.emplace_back(this->cell3->getrow() - 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() - 2);
+        v4.emplace_back(this->cell4->getrow());
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 0) && (!clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() - 2);
+        v1.emplace_back(this->cell1->getrow() - 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() + 1);
+        v2.emplace_back(this->cell2->getrow());
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol());
+        v3.emplace_back(this->cell3->getrow() - 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() - 1);
+        v4.emplace_back(this->cell4->getrow() - 2);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 1) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() - 1);
+        v1.emplace_back(this->cell1->getrow());
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() + 2);
+        v2.emplace_back(this->cell2->getrow() + 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() + 1);
+        v3.emplace_back(this->cell3->getrow());
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol());
+        v4.emplace_back(this->cell4->getrow() - 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 1) && (!clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() + 1);
+        v1.emplace_back(this->cell1->getrow() - 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol());
+        v2.emplace_back(this->cell2->getrow() + 2);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() + 1);
+        v3.emplace_back(this->cell3->getrow() + 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() + 2);
+        v4.emplace_back(this->cell4->getrow());
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 2) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol());
+        v1.emplace_back(this->cell1->getrow() - 2);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() - 1);
+        v2.emplace_back(this->cell2->getrow() + 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol());
+        v3.emplace_back(this->cell3->getrow());
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() + 1);
+        v4.emplace_back(this->cell4->getrow() - 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 2) && (!clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() + 1);
+        v1.emplace_back(this->cell1->getrow());
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() - 2);
+        v2.emplace_back(this->cell2->getrow() - 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() - 1);
+        v3.emplace_back(this->cell3->getrow());
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol());
+        v4.emplace_back(this->cell4->getrow() + 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 3) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() + 2);
+        v1.emplace_back(this->cell1->getrow() + 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() - 1);
+        v2.emplace_back(this->cell2->getrow());
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol());
+        v3.emplace_back(this->cell3->getrow() + 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() + 1);
+        v4.emplace_back(this->cell4->getrow() + 2);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol());
+        v1.emplace_back(this->cell1->getrow() + 2);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() + 1);
+        v2.emplace_back(this->cell2->getrow() - 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol());
+        v3.emplace_back(this->cell3->getrow());
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() - 1);
+        v4.emplace_back(this->cell4->getrow() + 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } 
     return result;
 }
 
@@ -268,6 +405,22 @@ char OBlock::getChar() {
 
 std::vector<std::vector<int>> OBlock::rotate(bool clockwise) {
     std::vector<std::vector<int>> result;
+    std::vector<int> v1;
+    v1.emplace_back(this->cell1->getcol());
+    v1.emplace_back(this->cell1->getrow());
+    std::vector<int> v2;
+    v2.emplace_back(this->cell2->getcol());
+    v2.emplace_back(this->cell2->getrow());
+    std::vector<int> v3;
+    v3.emplace_back(this->cell3->getcol());
+    v3.emplace_back(this->cell3->getrow());
+    std::vector<int> v4;
+    v4.emplace_back(this->cell4->getcol());
+    v4.emplace_back(this->cell4->getrow());
+    result.emplace_back(v1);
+    result.emplace_back(v2);
+    result.emplace_back(v3);
+    result.emplace_back(v4);
     return result;
 }
 
@@ -281,6 +434,41 @@ char SBlock::getChar() {
 
 std::vector<std::vector<int>> SBlock::rotate(bool clockwise) {
     std::vector<std::vector<int>> result;
+    if (((this->type == 0) || (this->type == 2)) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol());
+        v1.emplace_back(this->cell1->getrow());
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() - 1);
+        v2.emplace_back(this->cell2->getrow() + 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol());
+        v3.emplace_back(this->cell3->getrow() - 2);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() - 1);
+        v4.emplace_back(this->cell4->getrow() - 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol());
+        v1.emplace_back(this->cell1->getrow());
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() + 1);
+        v2.emplace_back(this->cell2->getrow() - 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol());
+        v3.emplace_back(this->cell3->getrow() + 2);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() + 1);
+        v4.emplace_back(this->cell4->getrow() + 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } 
     return result;
 }
 
@@ -294,6 +482,41 @@ char ZBlock::getChar() {
 
 std::vector<std::vector<int>> ZBlock::rotate(bool clockwise) {
     std::vector<std::vector<int>> result;
+    if (((this->type == 0) || (this->type == 2)) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() + 1);
+        v1.emplace_back(this->cell1->getrow() - 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol());
+        v2.emplace_back(this->cell2->getrow());
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() - 1);
+        v3.emplace_back(this->cell3->getrow() - 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() - 2);
+        v4.emplace_back(this->cell4->getrow());
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() - 1);
+        v1.emplace_back(this->cell1->getrow() + 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol());
+        v2.emplace_back(this->cell2->getrow());
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() + 1);
+        v3.emplace_back(this->cell3->getrow() + 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() + 2);
+        v4.emplace_back(this->cell4->getrow());
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    }
     return result;
 }
 
@@ -307,6 +530,143 @@ char TBlock::getChar() {
 
 std::vector<std::vector<int>> TBlock::rotate(bool clockwise) {
     std::vector<std::vector<int>> result;
+    if ((this->type == 0) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() + 1);
+        v1.emplace_back(this->cell1->getrow() - 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol());
+        v2.emplace_back(this->cell2->getrow());
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() - 1);
+        v3.emplace_back(this->cell3->getrow() + 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() - 1);
+        v4.emplace_back(this->cell4->getrow() - 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 0) && (!clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol());
+        v1.emplace_back(this->cell1->getrow() + 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol());
+        v2.emplace_back(this->cell2->getrow());
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() - 2);
+        v3.emplace_back(this->cell3->getrow() - 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol());
+        v4.emplace_back(this->cell4->getrow() - 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 1) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() + 1);
+        v1.emplace_back(this->cell1->getrow() + 2);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol());
+        v2.emplace_back(this->cell2->getrow() + 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() - 1);
+        v3.emplace_back(this->cell3->getrow());
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() + 1);
+        v4.emplace_back(this->cell4->getrow());
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 1) && (!clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() - 1);
+        v1.emplace_back(this->cell1->getrow() + 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol());
+        v2.emplace_back(this->cell2->getrow());
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() + 1);
+        v3.emplace_back(this->cell3->getrow() - 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() + 1);
+        v4.emplace_back(this->cell4->getrow() + 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 2) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() - 2);
+        v1.emplace_back(this->cell1->getrow() - 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() - 1);
+        v2.emplace_back(this->cell2->getrow() - 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol());
+        v3.emplace_back(this->cell3->getrow() - 2);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol());
+        v4.emplace_back(this->cell4->getrow());
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 2) && (!clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() - 1);
+        v1.emplace_back(this->cell1->getrow() - 2);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol());
+        v2.emplace_back(this->cell2->getrow() - 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() + 1);
+        v3.emplace_back(this->cell3->getrow());
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol() - 1);
+        v4.emplace_back(this->cell4->getrow());
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else if ((this->type == 3) && (clockwise)) {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol());
+        v1.emplace_back(this->cell1->getrow() - 1);
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() + 1);
+        v2.emplace_back(this->cell2->getrow());
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol() + 2);
+        v3.emplace_back(this->cell3->getrow() + 1);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol());
+        v4.emplace_back(this->cell4->getrow() + 1);
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } else {
+        std::vector<int> v1;
+        v1.emplace_back(this->cell1->getcol() + 2);
+        v1.emplace_back(this->cell1->getrow());
+        std::vector<int> v2;
+        v2.emplace_back(this->cell2->getcol() + 1);
+        v2.emplace_back(this->cell2->getrow() + 1);
+        std::vector<int> v3;
+        v3.emplace_back(this->cell3->getcol());
+        v3.emplace_back(this->cell3->getrow() + 2);
+        std::vector<int> v4;
+        v4.emplace_back(this->cell4->getcol());
+        v4.emplace_back(this->cell4->getrow());
+        result.emplace_back(v1);
+        result.emplace_back(v2);
+        result.emplace_back(v3);
+        result.emplace_back(v4);
+    } 
     return result;
 }
 

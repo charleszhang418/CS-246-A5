b74aff428d1c1cf0c12176244a7d5cbf6ba1eecf
Charles Zhang <charleszhang418@hotmail.com>
2022-04-03 21:04:59 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -138,7 +138,6 @@ int main (int argc, char *argv[]) {
         char p_B1;
         char p_B2;
 
-        if (b1.)
         
         Difficulty *curBlock1 = b1->generateNewBlock('T', 4);
         Difficulty *curBlock2 = b2->generateNewBlock('L', 2);
